21cb123e87beb91ac21721717a758136aa920d17
Davide Buoso <daviibuoso@gmail.com>
2025-05-12 11:09:08 +0200

    initial commit

---
+++ UI/controller.py
@@ -0,0 +1,27 @@
+import flet as ft
+
+
+class Controller:
+    def __init__(self, view, model):
+        # the view, with the graphical elements of the UI
+        self._view = view
+        # the model, which implements the logic of the program and holds the data
+        self._model = model
+        self._listYear = []
+        self._listColor = []
+
+    def fillDD(self):
+        pass
+
+
+    def handle_graph(self, e):
+        pass
+
+
+
+    def fillDDProduct(self):
+        pass
+
+
+    def handle_search(self, e):
+        pass
