ed755c0deeed227da5c9195b23b24ef5291120b2
simo813 <simoneluly08@gmail.com>
2025-05-14 18:45:33 +0200

    3

--- database/DAO.py
+++ database/DAO.py
@@ -54,3 +54,28 @@ class DAO():
             if conn:
                 conn.close()
         return result
+
+    @staticmethod
+    def getConnectionsOfColor(color):
+        result = []
+        try:
+            conn = DBConnect.get_connection()
+            cursor = conn.cursor(dictionary=True)
+            query = """ 
+                        select distinct (gp.Product_number), gds.Retailer_code , day(gds.`Date`) as day, month (gds.`Date`) as mounth 
+                        from go_products gp, go_daily_sales gds 
+                        where gp.Product_color = COALESCE(%s, gp.Product_color) and gp.Product_number = gds.Product_number and year (gds.`Date`) = COALESCE(%s, year (gds.`Date`))
+                    """
+            cursor.execute(query, (color,))
+
+            for row in cursor:
+                result.append((row["Product_number"], row["Retailer_code"], row["day"], row["mounth"]))
+
+        except Exception as e:
+            print(f"Error fetching colors: {e}")
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                conn.close()
+        return result
